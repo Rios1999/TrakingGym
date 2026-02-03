ce21433a610897c01fc2e5dd412b2461b49fd8a1
Rios1999 <joseriosjimenez1@gmail.com>
2026-02-03 16:02:51 +0100

    mejoras

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Sube el límite a 1000 KB
+  },
 })
